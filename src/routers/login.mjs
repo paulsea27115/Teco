0db45a26f6471ee00b56fd53f81575d05f31d0fd
paulsea27115 <csb070115@gmail.com>
2024-01-08 15:45:18 +0900

    [feat]

--- src/routers/login.mjs
+++ src/routers/login.mjs
@@ -6,8 +6,18 @@ const method = 'post'
 const handler = async (req, res) => {
     const { id, password: originalPassword } = req.body
 
-    const user = await User.findOne()
+    const user = await User.findOne({
+        id,
+        password: encryptPassword(originalPassword, id)
+    })
+
+    if (user === null) {
+        return res.redirect('/login?message=아이디와 비밀번호가 일치하지 앖습니다.')
+    }
     
-}
+    req.session._id = user._id.toString()
+    
+    return res.redirect('/')
+} 
 
 export { path, method, handler }
